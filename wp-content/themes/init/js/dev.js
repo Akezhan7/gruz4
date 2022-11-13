cc62510d625edb5a6d70ac9f25311298b5b0a734
Saduov <77147286+Akezhan7@users.noreply.github.com>
2022-11-13 20:57:14 +0600

    проба

--- wp-content/themes/init/js/dev.js
+++ wp-content/themes/init/js/dev.js
@@ -1,7 +1,14 @@
-const popmodal = document.querySelector ('.popup');
+const popmodal = document.querySelector ('.popup2');
+const closemodal = document.querySelector ('.close-popup2');
 
+closemodal.addEventListener ('click', e => {
+    popmodal.style.display = 'none';
+})
+document.querySelector ('.btn2').addEventListener ('click', e => {
+    popmodal.style.display = 'none';
+})
 function modaladd12 () {
     popmodal.style.display = 'block';
 };
 
-setTimeout (modaladd12, 3000); 
+setTimeout (modaladd12, 6000); 
